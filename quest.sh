1b9caae980793d16f749410f0a92dbb62a545f14
Aaron C Spike <aaron@ekips.org>
2012-07-01 00:32:19 +0000

    Make quest.sh more hygenic - Don't reinitialize venv if it already exists - Don't create a nested shell if venv is already active

--- quest.sh
+++ quest.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
-python bootstrap.py
-source venv/bin/activate
+ORIG_DIR=$PWD
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_VENV=$VIRTUAL_ENV
+
+cd $SCRIPT_DIR
+
+if [ ! -e "$SCRIPT_DIR/venv" ] ; then
+    echo "initializing virtualenv in $SCRIPT_DIR/venv"
+    python bootstrap.py
+else
+    echo "$SCRIPT_DIR/venv exists"
+    echo "skipping virtualenv initialization"
+fi
+
+if [ "$CURRENT_VENV" != "$SCRIPT_DIR/venv" ] ; then
+    source venv/bin/activate
+fi
+
 python quest.py init provision
-/bin/bash --rcfile venv/bin/activate
+
+if [ "$CURRENT_VENV" != "$SCRIPT_DIR/venv" ] ; then
+    echo "entering a nested bash"
+    echo "please type \`exit\` to return to your normally scheduled shell"
+    /bin/bash --rcfile venv/bin/activate
+fi
+
+cd $ORIG_DIR
 
